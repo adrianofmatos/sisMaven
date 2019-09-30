321d242954fd34fb0c6ab753ce458bccde6b3986
Adriano <adrianofalcao@gmail.com>
2019-09-29 21:16:14 -0300

    Testando JSF

---
+++ src/main/java/managedBean/UsuarioPessoaManagedBean.java
@@ -0,0 +1,42 @@
+package managedBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
+@ManagedBean(name = "usuarioPessoaManagedBean")
+@ViewScoped
+public class UsuarioPessoaManagedBean {
+
+	private UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
+	private DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<>();
+	private List<UsuarioPessoa> list = new ArrayList<UsuarioPessoa>();
+
+	public UsuarioPessoa getUsuarioPessoa() {
+		return usuarioPessoa;
+	}
+
+	public void setUsuarioPessoa(UsuarioPessoa usuarioPessoa) {
+		this.usuarioPessoa = usuarioPessoa;
+	}
+
+	public String salvar() {
+		daoGeneric.salvar(usuarioPessoa);
+		return "";
+	}
+	
+	public String novo() {
+		usuarioPessoa = new UsuarioPessoa();
+		return "";
+	}
+	
+	public List<UsuarioPessoa> getList() {
+		list = daoGeneric.listar(UsuarioPessoa.class);
+		return list;
+	}
+}
